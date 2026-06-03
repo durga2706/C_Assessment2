ee389a08737510ac1184cc9619b144b2d2511390
durga2706 <durkadevi0610@gmail.com>
2026-06-03 10:33:06 +0530

    Create c_problem1.c

---
+++ c_problem1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int num, result;
+    printf("enter the number:");
+    scanf("%d",&num);
+    result = (num == 50);
+    printf("%d", result);
+    return 0;
+}
